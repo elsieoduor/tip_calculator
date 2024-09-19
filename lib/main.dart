10b7233d1701f22ff753c7bc5130c546dd52aecd
Elsie Oduor <eoatieno@gmail.com>
2024-09-19 12:49:28 +0300

    Refactored main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tip_calculator/widgets/person_counter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,28 +29,95 @@ class YuTip extends StatefulWidget {
 }
 
 class _YuTipState extends State<YuTip> {
+  int _personCount = 1;
+
+  void increment() {
+    setState(() {
+      _personCount = _personCount + 1;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      if (_personCount > 0) {
+        _personCount--;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    final style = theme.textTheme.titleMedium!.copyWith(
+        color: theme.colorScheme.onPrimary, fontWeight: FontWeight.bold);
     return Scaffold(
       appBar: AppBar(
         title: const Text('YuTip'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            padding: const EdgeInsets.all(18),
-            decoration: BoxDecoration(
-              color:Colors.deepPurple,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child:const Column(
-              children: [
-                Text('Total Per Person'),
-                Text('\$20.60')
-              ],
-            ))
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.inversePrimary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Total Per Person',
+                      style: style,
+                    ),
+                    Text(
+                      '\$20.60',
+                      style: style.copyWith(
+                          color: theme.colorScheme.onPrimary,
+                          fontSize: theme.textTheme.displaySmall?.fontSize),
+                    )
+                  ],
+                )),
+          ),
+          //Form
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  border:
+                      Border.all(color: theme.colorScheme.primary, width: 2),
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.attach_money),
+                        labelText: 'Bill Amount',
+                      ),
+                      keyboardType: TextInputType.number,
+                      onChanged: (String value) {},
+                    ),
+                    //Split Bill area
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Split",
+                          style: theme.textTheme.titleMedium,
+                        ),
+                        PersonCounter(theme: theme, personCount: _personCount, onDecrement: decrement, onIncrement: increment,)
+                      ],
+                    )
+                  ],
+                )),
+          )
         ],
       ),
     );
   }
 }
+
+
