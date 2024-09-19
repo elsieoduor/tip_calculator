6e353c77b164303a699806bb3bb92167980302c5
Elsie Oduor <eoatieno@gmail.com>
2024-09-19 13:44:48 +0300

    Created total per person and modified person counter

--- lib/widgets/person_counter.dart
+++ lib/widgets/person_counter.dart
@@ -4,7 +4,9 @@ class PersonCounter extends StatelessWidget {
   const PersonCounter({
     super.key,
     required this.theme,
-    required int personCount, required this.onDecrement, required this.onIncrement,
+    required int personCount,
+    required this.onDecrement,
+    required this.onIncrement,
   }) : _personCount = personCount;
 
   final ThemeData theme;
@@ -15,20 +17,33 @@ class PersonCounter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        IconButton(
-          onPressed: onDecrement,
-          icon: const Icon(Icons.remove),
-          color: theme.colorScheme.primary,
-        ),
         Text(
-          "$_personCount",
+          'Split',
           style: theme.textTheme.titleMedium,
         ),
-        IconButton(
-          onPressed: onIncrement,
-          icon: const Icon(Icons.add),
-          color: theme.colorScheme.primary,
+        Row(
+          children: [
+            IconButton(
+              onPressed: onDecrement,
+              icon: Icon(
+                Icons.remove,
+                color: theme.colorScheme.primary,
+              ),
+            ),
+            Text(
+              '$_personCount',
+              style: theme.textTheme.titleMedium,
+            ),
+            IconButton(
+              onPressed: onIncrement,
+              icon: Icon(
+                Icons.add,
+                color: theme.colorScheme.primary,
+              ),
+            ),
+          ],
         ),
       ],
     );
